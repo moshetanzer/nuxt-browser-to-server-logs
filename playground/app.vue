9ff377ead8ba17356ee5d371dbb755f9d36332c3
Moshe Tanzer <tanzermt@gmail.com>
2025-06-26 13:48:19 +0200

    chore: update playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     Nuxt module playground!
+    <button @click="test">
+      Test Console Logs
+    </button>
   </div>
 </template>
 
 <script setup>
+function test() {
+  console.log('This is a test log message')
+  console.warn('This is a test warning message')
+  console.error('This is a test error message')
+  console.info('This is a test info message')
+  console.debug('This is a test debug message')
+  console.log(`User ${'Moshe'} has ${2 + 3} messages at ${new Date().toISOString()}`)
+}
 console.log('Final test: ', '&amp;lt;script&amp;gt;alert("xss")&amp;lt;/script&amp;gt;')
 console.warn('This is the Nuxt module playground!')
 console.log('You can use this to test your Nuxt modules in isolation.')
